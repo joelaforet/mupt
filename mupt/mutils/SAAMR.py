8d4cdd32f40852c6235bbf395b77c71d6be68b51
joelaforet <joelaforetjr@yahoo.com>
2026-01-21 17:17:51 -0700

    Added place for SAAMR utilities to live

---
+++ mupt/mutils/SAAMR.py
@@ -0,0 +1,7 @@
+"""Utilities for dealing with systems compliant with the Standard All-Atom Molecular Representation
+   namely Primitives that conform to the organization of [Universe -> Molecules -> Repeat-Units -> Atoms]"""
+   
+__author__ = 'Joseph R. Laforet Jr.'
+__email__ = 'jola3134@colorado.edu'
+
+
