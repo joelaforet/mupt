321919c8aec06e02d465c7bd88339649b7574cc7
joelaforet <joelaforetjr@yahoo.com>
2026-02-20 15:04:17 -0700

    ValueError is now thrown if univprim is not SAAMR
    ValueError should be thrown, as this is on the user for not providing a
    SAAMR compliant primitive. I may add a custom ValueError eventually, but
    for now this will do.

--- mupt/interfaces/mdanalysis/exporters.py
+++ mupt/interfaces/mdanalysis/exporters.py
@@ -123,7 +123,12 @@ def primitive_to_mdanalysis(univprim : Primitive,
     >>> LOGGER.info(f"Number of segments: {universe.segments.n_segments}")
     """
     
-    assert _is_SAAMR_compliant(univprim), "[Under Construction] Primitive must be ordered according to: universe -> chains -> residues -> atoms"
+    
+    if not _is_SAAMR_compliant(univprim):
+        raise ValueError("Primitive is not SAAMR-compliant. Expected a hierarchy ordered as "  
+        "universe -> chains -> residues -> atoms. Ensure that the input "  
+        "Primitive is constructed or reordered to follow this hierarchy "  
+        "before calling primitive_to_mdanalysis().")
 
     # ----------------------------
     # Containers (allow duplicates)
