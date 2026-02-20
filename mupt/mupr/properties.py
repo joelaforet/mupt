1c605f28b4066c2a7c57d8ba05063b0c7519b43e
joelaforet <joelaforetjr@yahoo.com>
2026-02-20 15:15:47 -0700

    Created new file to house utilities about Primitive properties
    This file stores functionality associated with maintaining properties on
    certain types of primitives. For example, if we want to determine
    whether a primitive is all-atom exportable, or if we want to query whether a
    primitive is SAAMR-compliant.

---
+++ mupt/mupr/properties.py
@@ -0,0 +1,22 @@
+"""Utility methods for Primitive objects that determine various properties like exportability
+   or adherence to a canonical organization (i.e. SAAMR-compliant primitives)"""
+   
+__author__ = 'Joseph R. Laforet Jr.'
+__email__ = 'jola3134@colorado.edu'
+
+from mupr.primitives import Primitive
+
+
+"""Utilities for dealing with systems compliant with the Standard All-Atom Molecular Representation
+   namely Primitives that conform to the organization of [Universe -> Molecules -> Repeat-Units -> Atoms]"""
+
+def _is_SAAMR_compliant(prim : Primitive) -> bool:
+    """
+    Check whether a Primitive hierarchy is organized
+    as universe -> molecule -> repeat-unit -> atom
+    """
+    
+    return all(
+        leaf.is_atom and (leaf.depth == 3)        
+            for leaf in prim.leaves
+    )
